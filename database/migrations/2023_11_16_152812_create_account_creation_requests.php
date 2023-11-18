8f66ae27fff1949fd09bbee5a7887b04c7f67de9
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-11-18 11:59:21 +0200

    fixed create transactions and account creatin request migrations

--- database/migrations/2023_11_16_152812_create_account_creation_requests.php
+++ database/migrations/2023_11_16_152812_create_account_creation_requests.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccountCreationRequestsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -22,5 +22,5 @@ class CreateAccountCreationRequestsTable extends Migration
     {
         Schema::dropIfExists('account_creation_requests');
     }
-}
+};
 
