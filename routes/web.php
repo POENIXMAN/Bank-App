7e423e89ade206bf6b750f8d78291940da1f979f
POENIXMAN <userr100p@gmail.com>
2023-11-18 12:30:56 +0200

    added login and registration with security

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,45 +8,14 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-
-Route::get('/index', function () {
-    return view('index');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/main-menu', function () {
-    return view('main_menu');
-});
-
-Route::get('/create-acc', function () {
-    return view('form_create_account');
-});
-
-Route::post('/submit-form-create', [App\Http\Controllers\Controller::class, 'createAccount']);
-
-Route::get('/add-credit', function () {
-    return view('form_add_credit');
-});
-
-Route::get('/display-acc', [App\Http\Controllers\Controller::class, 'getAccounts']);
-
-Route::post('/submit-form-add-credit', [App\Http\Controllers\Controller::class, 'addCredit']);
-
 
 Route::controller(LoginRegisterController::class)->group(function() {
     Route::get('/register', 'register')->name('register');
